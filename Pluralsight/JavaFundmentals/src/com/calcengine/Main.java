b207520e4112156678a44c87b17e0eb8959ec14a
Joshua <joshua.hiquet@salesforce.com>
2018-07-25 21:29:17 -0700

    Module 8: Method Overloading

--- Pluralsight/JavaFundmentals/src/com/calcengine/Main.java
+++ Pluralsight/JavaFundmentals/src/com/calcengine/Main.java
@@ -3,10 +3,6 @@ package com.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-//        double[] leftVals = {100.0d, 25.0d, 225.0d, 111.0d};
-//        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-//        char[] opCodes = {'d', 'a', 's', 'm'};
-//        double[] results = new double[opCodes.length];
         MathEquation[] equations = new MathEquation[4];
         equations[0] = new MathEquation(100.0d, 50.0d, 'd');
         equations[1] = new MathEquation(100.0d, 50.0d, 'a');
@@ -18,16 +14,17 @@ public class Main {
             equation.execute();
             System.out.println("Result: " + equation.getResult());
         }
-    }
 
-    // Turned this create method into a constructor
-//    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-//        MathEquation equation = new MathEquation();
-//
-//        equation.setLeftVal(leftVal);
-//        equation.setRightVal(rightVal);
-//        equation.setOpCode(opCode);
-//
-//        return equation;
-//    }
+        System.out.println();
+        System.out.println("Using Overloads");
+
+        double leftDouble = 9.0d;
+        double rightDouble = 4.0d;
+
+        MathEquation equationOverload = new MathEquation('d');
+        equationOverload.execute(leftDouble, rightDouble);
+        System.out.println("Overload result = ");
+        System.out.println(equationOverload.getResult());
+
+    }
 }
